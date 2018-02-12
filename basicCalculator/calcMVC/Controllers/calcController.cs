3ade9baf5d02499213f96a953cab6f64c9e3b111
Fahim Afser <fahimh02@gmail.com>
2018-02-12 23:12:07 +0600

    Form submit action handled in controller

---
+++ basicCalculator/calcMVC/Controllers/calcController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace calcMVC.Controllers
+{
+    public class calcController : Controller
+    {
+        // GET: calc
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult total(string operation, int num1,int num2)
+        {
+            int total = 0;
+            switch (operation) {
+                case "+" :
+                    total = num1 + num2;
+                    break;
+
+                case "-" :
+                    total = num1 - num2;
+                    break;
+
+                case "*" :
+                    total = num1 * num2;
+                    break;
+
+                case  "/" :
+                    total = num1 / num2;
+                    break;
+            }
+            ViewBag.num1 = num1;
+            ViewBag.num2 = num2;
+            ViewBag.operation = operation;
+            ViewBag.result = total;
+            return View();
+        }
+    }
+}
---
+++ basicCalculator/calcMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="calcMVC.MvcApplication" Language="C#" %>
